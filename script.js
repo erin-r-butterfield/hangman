503c59bc27310900a850dfb5e1b06963eac27bcf
erin-r-butterfield <erin.r.butterfield@gmail.com>
2023-01-06 21:27:34 +0000

    Runable game

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
-var shown_word = document.getElementById("shown_word")
-// shown_word.style.display = 'none';
+var shown_word = document.getElementById("shown_word");
+var left_div = document.getElementById("left_div");
+var hangman = [];
 
-var left_div = document.getElementById("left_div")
-var hangman = document.getElementById("hangman_image")
-
+// Hangman Steps
+hangman[0] = document.getElementById("vertical_1");
+hangman[1] = document.getElementById("horizontal_1");
+hangman[2] = document.getElementById("horizontal_2");
+hangman[3] = document.getElementById("vertical_2"); 
+hangman[4] = document.getElementById("head");
+hangman[5] = document.getElementById("body");
+hangman[6] = document.getElementById("arm_1");
+hangman[7] = document.getElementById("arm_2");
+hangman[8] = document.getElementById("leg_1");
+hangman[9] = document.getElementById("leg_2");
+hangman[10] = document.getElementById("face");
 
 var mywords = ["phylogenetics", "matrix", "cluster", "bayesian", "bioinformatics"];
 var current_word = mywords[(Math.floor(Math.random() * mywords.length))];
 var display_word = "";
-// const wordArr = current_word.split("");
+var attempts = 0;
+var available = document.getElementById("letters")
+var letters = ["A" ,"B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 for (let i = 0; i < current_word.length; i++){
     display_word += "_ ";
 }
 
-var word = document.getElementById("word")
+var word = document.getElementById("word");
 
 function game_start(event){
     left_div.style.display = 'none';
-    hangman.style.display = 'none';
+    hangman.forEach(element => {
+        element.style.display = 'none';
+    });
     shown_word.style.display = 'block';
     word.innerHTML = display_word;
+    available.innerHTML = letters;
     document.addEventListener('keypress', guess);
     }
 
 function guess(event){
-    // var x = event.key;
-    // match to character/s in string
     console.log(event.key);
     if (current_word.includes(event.key)){
         display_word = correct(event.key)
-        console.log('yay you guessed correctly')
-        word.innerHTML = display_word
+        console.log('yay you guessed correctly');
+        word.innerHTML = display_word;
+        if (display_word.replace(/\s/g, '') == current_word){
+            YouWin()
+        }
+    } else if (attempts < (hangman.length - 1)){
+        console.log('you suck, '+event.key+' is not in '+current_word);
+        hangman[attempts].style.display = 'block';
+        console.log('attempt number '+attempts);
+        attempts++;
+        console.log('now at '+attempts);
     } else {
-        console.log('you suck, '+event.key+' is not in '+current_word)
-        // false = reveal next bodypart
-    } 
+        console.log("YOU REALLY SUCK, GAME OVER")
+        hangman[attempts].style.display = 'block';
+        GameOver()
+    }
 }
 
 
@@ -47,23 +70,15 @@ function matchall(x){
             console.log(current_word[i]+' matches '+x.toLowerCase());
             matches.push(i)
             console.log("matches = " +matches)
-          } else {
-              console.log("my if statement didn't work :(")
-          }
+        } else {
+            console.log("my if statement didn't work :(")
+        }
     }
-    // var index = 0
-    // do {
-    //     index = current_word.indexOf(x, index);
-    //     matches.push(index);
-
-    // }
-    // while (index !== -1);
     return matches;
 }
 
 function update_string(str,index,chr){
     // adapted from https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
-    // if(index > str.length-1) return str;
     console.log(index)
     return str.substring(0,(index*2)) + chr + str.substring((index*2)+1);
 }
@@ -79,6 +94,26 @@ function correct(x){
     return display_word
 }
 
-// function incorrect(){
+function GameOver(){
+    setTimeout(function(){
+        if(confirm("Game Over \nYour word was " + current_word + "\n\nClick OK to play again.")==true){
+        location.reload();
+        }
+    },500);
+}
 
-// }
+function YouWin(){
+    setTimeout(function(){
+        if(confirm("CONGRATULATIONS - You Win\n\nClick OK to play again.")==true){
+            location.reload();
+        }
+    }, 500)
+}
+
+// TODO:
+// 1. Show available letters
+// 2. Expand available words
+// 3. Include phrases?
+// 4. Sanitise inputs so only legit characters allowed?
+// 5. Allow repeat attempts of the same letter?
+// 6. Document code
